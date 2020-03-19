073ee37db3cab8f5385cccb893597818e2afcde2
Adam Kubiczek <adam.kubiczek@kissdigital.com>
2020-03-19 11:38:58 +0100

    Migrated to Laravel 7

--- config/database.php
+++ config/database.php
@@ -35,6 +35,21 @@ return [
 
     'connections' => [
 
+        'system' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'tenancy'),
+            'username' => env('DB_USERNAME', 'tenancy'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
